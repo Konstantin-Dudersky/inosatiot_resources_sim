fef256975caf2c872fa8d094775e4084429ab98c
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2021-07-26 23:32:14 +0300

    add batch mode and command-line interface

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import datetime
+import getopt
+import sys
 import time
 
 import yaml
@@ -6,38 +9,140 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 import electricity
 
-with open('../config_inosatiot_resources_sim.yaml') as stream:
-    config = yaml.safe_load(stream)
 
-TOKEN = config['influxdb']['token']
-ORG = config['influxdb']['org']
-URL = config['influxdb']['url']
-BUCKET = config['influxdb']['bucket']
+def progressBar(current, total, barLength=20):
+    percent = float(current) * 100 / total
+    arrow = '-' * int(percent / 100 * barLength - 1) + '>'
+    spaces = ' ' * (barLength - len(arrow))
 
-client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
-write_api = client.write_api(write_options=SYNCHRONOUS)
+    print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
 
-# electricity
 
-energymeters = {}
-for e in config['electricity']:
-    label = e['label']
-    if label not in energymeters:
-        energymeters[label] = electricity.SimElectricity(
-            label=label,
-            p_base=e['p']['base'], p_var=e['p']['var'], p_delay=e['p']['delay'],
-            q_base=e['q']['base'], q_var=e['p']['var'], q_delay=e['p']['delay'],
-        )
+if __name__ == "__main__":
 
-while True:
+    opts, args = getopt.getopt(sys.argv[1:], "hm:b:", ['help', 'mode=', 'start=', 'stop='])
 
-    record = []
-    for key in energymeters:
-        record.extend(energymeters[key].cycle())
+    mode = None
+    start = None
+    stop = None
 
-    write_api.write(
-        bucket=BUCKET,
-        record=record
-    )
+    for opt, arg in opts:
 
-    time.sleep(10)
+        if opt in ['-h', '--help']:
+            print(f"""
+Simulator
+
+arguments:
+    -h, --help : show help
+    --mode (required) : mode
+        rt - cyclical execute in real-time
+        batch - wtite batch data in range (--start, --stop)
+    --start <datetime in isoformat> (required for batch mode):
+    --stop <datetime in isoformat> (optional for batch mode):
+    
+examples:
+    venv/bin/python3 main.py --mode rt
+    venv/bin/python3 main.py --mode batch --start 2021-01-01T00:00:00+03:00
+    venv/bin/python3 main.py --mode batch --start 2021-01-01T00:00:00+03:00 --stop 2022-01-01T00:00:00+03:00
+""")
+            sys.exit()
+        elif opt == '--mode':
+            if arg in ['rt', 'batch']:
+                mode = arg
+
+        elif opt == '--start':
+            try:
+                start = datetime.datetime.fromisoformat(arg)
+            except ValueError as ve:
+                print(f'Invalid --start value:\n{ve}')
+                sys.exit()
+
+        elif opt == '--stop':
+            try:
+                stop = datetime.datetime.fromisoformat(arg)
+            except ValueError as ve:
+                print(f'Invalid --stop value:\n{ve}')
+                sys.exit()
+
+    if mode is None:
+        print(f"Invalid --mode value, see --help")
+        sys.exit()
+    elif mode == 'rt':
+        pass
+    elif mode == 'batch':
+        if start is None:
+            print(f"Value --start required for batch mode, exit")
+            sys.exit()
+        if stop is None:
+            stop = datetime.datetime.now().astimezone()
+
+        if start.tzname() is None:
+            local_tz = datetime.datetime.now().astimezone().tzinfo
+            start = start.astimezone(local_tz)
+
+        if stop.tzname() is None:
+            local_tz = datetime.datetime.now().astimezone().tzinfo
+            stop = stop.astimezone(local_tz)
+
+        print(f"Start batch mode, start:{start.isoformat()}, stop:{stop.isoformat()}")
+
+    batch_ts = start
+
+    # sys.exit()
+
+    with open('../config_inosatiot_resources_sim.yaml') as stream:
+        config = yaml.safe_load(stream)
+
+    TOKEN = config['influxdb']['token']
+    ORG = config['influxdb']['org']
+    URL = config['influxdb']['url']
+    BUCKET = config['influxdb']['bucket']
+
+    client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    # electricity
+    energymeters = {}
+    for e in config['electricity']:
+        label = e['label']
+        if label not in energymeters:
+            energymeters[label] = electricity.SimElectricity(
+                label=label,
+                p_base=e['p']['base'], p_var=e['p']['var'], p_delay=e['p']['delay'],
+                q_base=e['q']['base'], q_var=e['p']['var'], q_delay=e['p']['delay'],
+                now=datetime.datetime.now() if mode == 'rt' else batch_ts
+            )
+
+    if mode == 'rt':
+        while True:
+            record = []
+            for key in energymeters:
+                record.extend(energymeters[key].cycle(datetime.datetime.now()))
+
+            write_api.write(
+                bucket=BUCKET,
+                record=record
+            )
+
+            time.sleep(10)
+    elif mode == 'batch':
+        while True:
+            record = []
+            for i in range(10000):
+                for key in energymeters:
+                    record.extend(energymeters[key].cycle(batch_ts))
+
+                batch_ts += datetime.timedelta(seconds=10)
+                if batch_ts >= stop:
+                    break
+
+            progressBar((batch_ts - start).total_seconds(), (stop - start).total_seconds(), barLength=50)
+
+            write_api.write(
+                bucket=BUCKET,
+                record=record
+            )
+
+            if batch_ts >= stop:
+                print("\nBatch execution finished")
+                sys.exit()
